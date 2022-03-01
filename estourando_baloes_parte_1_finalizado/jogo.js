3b316b76479817798af66b37e23ec6f0cdf7edd3
Aline Antonucci <alineantonucci@icloud.com>
2022-03-01 13:57:59 -0300

    iniciando jogo de estoura baloes com js

---
+++ estourando_baloes_parte_1_finalizado/jogo.js
@@ -0,0 +1,51 @@
+function iniciaJogo(){
+    var url = window.location.search;
+   
+    var nivel_jogo = url.replace("?", "");
+
+    var tempo_segundos = 0;
+
+    if(nivel_jogo == 1){//1 fácil ->120segundos
+        tempo_segundos = 120;
+    }
+
+    if(nivel_jogo == 2){//2 normal ->60segundos
+        tempo_segundos = 60;
+    }
+
+    if(nivel_jogo == 3){//3 difícil ->30segundos
+        tempo_segundos = 30;
+    }
+
+    //1 fácil ->120segundos
+
+    //2 normal ->60segundos
+
+    //3 difícil ->30segundos
+
+    //inserindo segundos no span
+    document.getElementById('cronometro').innerHTML = tempo_segundos;
+
+    //quantidade de balões
+    var qtde_baloes =30;
+
+    cria_baloes(qtde_baloes);
+
+    //imprimir qtde balões inteiros
+    document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
+    //imprimir qtde balões estourados
+    document.getElementById('baloes_estourados').innerHTML = 0;
+}
+   
+
+function cria_baloes(qtde_baloes){
+
+    for(var i = 1; i <= qtde_baloes; i++){
+
+        var balao = document.createElement("img");
+        balao.src = 'imagens/balao_azul_pequeno.png';
+        balao.style.margin = '10px';
+
+        document.getElementById('cenario').appendChild(balao);
+    }
+}
